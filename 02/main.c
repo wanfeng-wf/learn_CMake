b13c61b3181897393267fa3a6f91b6e84e808e33
wanfeng-wf <2517514847@qq.com>
2026-02-24 16:09:45 +0800

    first commit

---
+++ 02/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    // C99 标准引入的循环内变量声明
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Hello C Step: %d\n", i);
+    }
+    return 0;
+}
---
+++ 03/src/hello.c
@@ -0,0 +1,7 @@
+#include "hello.h"
+#include <stdio.h>
+
+void say_hello()
+{
+    printf("Hello from src directory!\n");
+}
